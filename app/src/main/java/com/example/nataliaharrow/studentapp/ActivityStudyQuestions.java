c42b5fd2c6cf01e3c4e77e9fd741f24ea54ef9ce
natpas <nataliaharrow@gmail.com>
2017-11-15 00:15:01 -0500

    Added buttons

---
+++ app/src/main/java/com/example/nataliaharrow/studentapp/ActivityStudyQuestions.java
@@ -0,0 +1,13 @@
+package com.example.nataliaharrow.studentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityStudyQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_questions);
+    }
+}
