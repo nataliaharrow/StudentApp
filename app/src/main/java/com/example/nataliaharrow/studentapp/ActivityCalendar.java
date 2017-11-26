20d6cd80e38ed38acc2cb0b18a46114041bb77be
natpas <nataliaharrow@gmail.com>
2017-11-26 14:57:01 -0500

    CalendarView

--- app/src/main/java/com/example/nataliaharrow/studentapp/ActivityCalendar.java
+++ app/src/main/java/com/example/nataliaharrow/studentapp/ActivityCalendar.java
@@ -5,25 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class ActivityCalendar extends AppCompatActivity {
 
     private static final String TAG = "ActivityCalendar";
 
-    private CalendarView mCalendarView;
+    private CalendarView myCalendarView;
+    private TextView myDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        mCalendarView = (CalendarView)findViewById(R.id.calendarView);
+        myCalendarView = (CalendarView)findViewById(R.id.calendarView);
+        myDate = (TextView)findViewById(R.id.textView);
 
-        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        myCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-                String date = i + "/" + i1 + "/" + i2;
-                Log.d(TAG, "onSelectedDayChange: date" + date);
+                String date = (i1 + 1) + "/" + i2 + "/" + i;
+                myDate.setText(date);
+               // Log.d(TAG, "onSelectedDayChange: date" + date);
             }
         });
     }
